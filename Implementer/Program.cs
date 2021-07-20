1fef90b1df96803c88570c854d62c3da58cc6d24
new_user <new_user@gmail.com>
2021-07-20 22:03:15 +0300

    Добавьте файлы проекта.

---
+++ Implementer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace Implementer
+{
+    class Program
+    {
+        static void Config(ServiceCollection services)
+        {
+            services.AddScoped<Worker>(a => new Worker("Hi"));
+        }
+        static ServiceProvider CreateProvider()
+        {
+            var service = new ServiceCollection();
+            Config(service);
+            return service.BuildServiceProvider();
+        }
+       
+        static void Main(string[] args)
+        {
+
+            using (provider)
+            {
+                Initialization();
+            }
+        }
+        static void Initialization()
+        {
+           var res =  Worker.Current;
+            res.Write();
+            Console.ReadKey();
+        }
+        public static ServiceProvider provider { get; }
+        static Program()
+        {
+            provider = CreateProvider();
+        }
+    }
+}
---
+++ Implementer/Worker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementer
+{
+    class Worker: BaseFactory<Worker>
+    {
+        public void Write()
+        {
+            Console.WriteLine(text);
+        }
+        string text;
+        public Worker(string text)
+        {
+            this.text = text;
+        }
+    }
+}
